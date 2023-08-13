673706425e982a84293b30c50cf17e9c0418ea87
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2023-08-13 16:13:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (20.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,7 +7,18 @@ public:
         return dp[m][n]=helper(m-1, n, dp)+helper(m, n-1, dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return helper(m-1, n-1, dp);
+        // vector<vector<int>> dp(m, vector<int>(n,-1));
+        // return helper(m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++) dp[i][0]=1;        for(int i=0; i<n; i++) dp[0][i]=1;
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+        
     }
 };
