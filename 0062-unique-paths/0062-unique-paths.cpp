9a97b9a53a664473e6a623729c11212e38d648b6
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2023-08-13 16:49:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (76.74%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,20 +9,26 @@ public:
     int uniquePaths(int m, int n) {
         // vector<vector<int>> dp(m, vector<int>(n,-1));
         // return helper(m-1, n-1, dp);
-        vector<int> dp(m, 0);
-        // dp[0] = 1;
-        for(int i=0; i<m; i++) dp[i]=1;        
-        // for(int i=0; i<n; i++) dp[0][i]=1;
+//         vector<int> dp(m, 0);
+//         // dp[0] = 1;
+//         for(int i=0; i<m; i++) dp[i]=1;        
+//         // for(int i=0; i<n; i++) dp[0][i]=1;
 
-        for(int i=1; i<n; i++){
-            vector<int> temp(m, 0);
-            temp[0]= dp[0];
-            for(int j=1; j<m; j++){
-                temp[j] = temp[j-1]+dp[j];
-            }
-            dp=temp;
-        }
-        return dp[m-1];
+//         for(int i=1; i<n; i++){
+//             vector<int> temp(m, 0);
+//             temp[0]= dp[0];
+//             for(int j=1; j<m; j++){
+//                 temp[j] = temp[j-1]+dp[j];
+//             }
+//             dp=temp;
+//         }
+//         return dp[m-1];
+        int N = m+n-2;
+        int r = m-1;
+        double res=1;
+        for(int i=1; i<=r; i++)
+            res = res*(N-r+i)/i;
+        return int(res);
         
     }
 };
