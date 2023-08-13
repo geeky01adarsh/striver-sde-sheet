c466331029eaa8194770d97e86c52a00845f007b
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2023-08-13 17:13:05 +0530

    Time: 5 ms (28.33%), Space: 7.6 MB (81.36%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
-        // int mod = 1e9+7;
-        int n=mat.size(), m =mat[0].size();
-        vector<int> dp(m, 0);
-        // cout<<m<<endl;
-    for(int i=0; i<m; i++){
-        if(mat[0][i]==1) break;
-        dp[i]=1; 
-    }
-    for(int i=1; i<n; i++){
-        vector<int> temp(m, 0);
-        for(int j=0; j<m; j++){
-            // if(!i&&!j)continue;
-            // if(mat[i-1][j]!=-1) dp[i][j]=(dp[i][j]+dp[i-1][j])%mod;
-            if(mat[i][j]==1) continue;
-            if(mat[i-1][j]!=1) temp[j]=(temp[j]+dp[j]);
-            if(j>0 && mat[i][j-1]!=1) temp[j] = (temp[j]+temp[j-1]);
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int  n = grid.size(), m = grid[0].size();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+        vector<int> dp(n, 0);
+        dp[0]=1;
+        for(int i=1; i<n; i++) {
+            if(grid[i][0]==1) break; 
+            dp[i]=1;
         }
-        dp=temp;
-    }
-    return dp[m-1];
+        for(int i=1; i<m; i++){
+            vector<int> temp(n, 0);
+            if(grid[0][i]!=1) temp[0]= dp[0];
+            for(int j=1; j<n; j++){
+                if(grid[j][i]==1) continue;
+                temp[j] = temp[j-1]+dp[j];
+            }
+            dp=temp;
+        }
+        return dp[n-1];
     }
 };
